718b942f44d96b31ea26e7f0765b1a0c9de8c396
caitlin.ai/ <chiangcaitlin2003@gmail.com>
2023-03-06 16:53:24 +0800

    Skills WIP

---
+++ src/components/Skills.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import FadeInSection from './FadeInSection'
+import '../styles/Skills.css'
+
+class Skills extends Component {
+  render() {
+    const frontend_stack = [
+      'Javascript ES6+',
+      'React.js',
+      'Typescript',
+      'HTML',
+      'Material UI',
+      'CSS'
+    ]
+
+    const backend_stack = [
+      'Node.js',
+      'Express.js',
+      'Python 3',
+      'Flask'
+    ]
+
+    const database_stack = [
+      'MongoDB',
+      'MySQL',
+      'Firebase'
+    ]
+
+    const other_services = [
+      'Apollo GraphQL',
+      'Arduino',
+      'Amazon Web Services',
+      'Cloudinary',
+      'Netlify',
+      'Cypress.io'
+    ]
+
+    const currently_learning = [
+      'Flutter',
+      'Heroku'
+    ]
+
+    return (
+      <div id="skills">
+        
+      </div>
+    )
+  }
+}
+
+export default Skills
