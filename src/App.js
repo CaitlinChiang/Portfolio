552a3195838a09230cb934422fc92e575cec6b78
caitlin.ai/ <chiangcaitlin2003@gmail.com>
2023-03-06 16:44:47 +0800

    Initialize app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from './components/Home'
+import './App.css'
+import './styles/Global.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div id="content">
+        <Home />
+      </div>
+    </div>
+  )
+}
+
+export default App
