2daa7860203f10fad0c19f11b09d065e0b271541
caitlin.ai/ <chiangcaitlin2003@gmail.com>
2023-05-22 22:47:22 +0800

    Change icons & public_url dynamic

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,12 @@ import '../styles/Home.css'
 
 class Home extends Component {
   render() {
+    const publicUrl = process.env.PUBLIC_URL;
+
     return (
       <div id="intro">
         <div className='intro-left'>
-          <img class="curved-image" src={"http://localhost:3000/images/profile.jpg"} alt="Profile Image" />
+          <img class="curved-image" src={publicUrl + "/images/profile.jpg"} alt="Profile Image" />
         </div>
         <div className="intro-right">
           <div className="intro-name">
@@ -16,7 +18,7 @@ class Home extends Component {
 
           <div className="intro-caption"><span className="emphasize-description">I'm obsessed with building software.</span> Currently a student in the Singapore University of Technology and Design, I'm pursuing a degree in computer science mixed in with design and artificial intelligence. I am passionate about automating manual systems and crafting web-products that serve immense value to the community.</div>
           <button className="intro-contact intro-contact-resume">
-            <a href="http://localhost:3000/assets/Resume.pdf" download="caitlin-resume">Download CV</a>
+            <a href={publicUrl + "/assets/Resume.pdf"} download="caitlin-resume">Download CV</a>
           </button>
         </div>
       </div>
