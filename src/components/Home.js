a8b89f61a527c1b7c4dd1818137967acb1b78c48
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2023-05-23 17:09:23 +0800

    Change emphasis

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ class Home extends Component {
             <p className="intro-description">Full-Stack Developer</p>
           </div>
 
-          <div className="intro-caption"><span className="emphasize-description">I'm obsessed with building software.</span> Currently a student in the Singapore University of Technology and Design, I'm pursuing a degree in computer science mixed in with design and artificial intelligence. I am passionate about automating manual systems and crafting web-products that serve immense value to the community.</div>
+          <div className="intro-caption"><span className="emphasize-description">I'm obsessed with building software.</span> Currently a student in the Singapore University of Technology and Design, I'm pursuing a degree in computer science mixed in with design and artificial intelligence. I am <span className="emphasize-description">passionate</span> about <span className="emphasize-description">automating manual systems and crafting web-products</span> that serve immense value to the community.</div>
           <button className="intro-contact intro-contact-resume">
             <a href={publicUrl + "/assets/Resume.pdf"} download="caitlin-resume">Download CV</a>
           </button>
