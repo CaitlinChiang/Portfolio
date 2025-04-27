7824115ad15bc429fa6778302b4b3ff9f53656d6
caitlin.ai/ <chiangcaitlin2003@gmail.com>
2025-04-27 19:37:02 +0800

    edit introduction

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,10 +13,10 @@ class Home extends Component {
         <div className="intro-right">
           <div className="intro-name">
             <p className="intro-emphasize">I'm Caitlin Chiang.</p>
-            <p>A Full-Stack Software Engineer</p>
+            <p>An AI and Full-Stack Software Engineer</p>
           </div>
 
-          <div className="intro-caption"><span className="emphasize-description">I'm obsessed with building software.</span> Currently a student in the Singapore University of Technology and Design, I'm pursuing a degree in computer science mixed in with design and artificial intelligence. I am <span className="emphasize-description">passionate</span> about <span className="emphasize-description">automating manual systems and crafting web-products</span> that serve immense value to the community.</div>
+          <div className="intro-caption"><span className="emphasize-description">I'm obsessed with building software.</span> Currently a graduating senior in the Singapore University of Technology and Design, I'm pursuing a degree in Design and Artificial Intelligence, with minors in Computer Science and Digital Humanities. I am <span className="emphasize-description">passionate</span> about <span className="emphasize-description">automating manual systems and crafting web-products</span> that serve immense value to the community.</div>
           <button className="intro-contact intro-contact-resume">
             <a href={publicUrl + "/assets/Resume.pdf"} download="caitlin-resume">Download Resume</a>
           </button>
